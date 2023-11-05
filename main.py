0ee546b9b24fb22c6d2f354a06b2f1e408c27eae
cmyui <cmyuiosu@gmail.com>
2023-11-05 09:43:25 -0500

    more

--- main.py
+++ main.py
@@ -5,25 +5,27 @@ import fastapi
 import settings
 
 import uvicorn
-import app.logging
+import logger
 
-import app.exception_handling
+import exception_handling
 
 import atexit
 
-def main() -> int:
-    app.logging.configure_logging()
 
-    app.exception_handling.hook_exception_handlers()
-    atexit.register(app.exception_handling.unhook_exception_handlers)
+def main() -> int:
+    logger.configure_logging()
+
+    exception_handling.hook_exception_handlers()
+    atexit.register(exception_handling.unhook_exception_handlers)
 
     uvicorn.run(
-        "app.init_api:asgi_app",
-        reload=config.CODE_HOTRELOAD,
-        log_level=config.LOG_LEVEL,
+        "main:app",
+        reload=settings.CODE_HOTRELOAD,
+        log_level=settings.LOG_LEVEL,
         server_header=False,
         date_header=False,
-        port=config.APP_PORT,
+        host=settings.APP_HOST,
+        port=settings.APP_PORT,
         access_log=False,
     )
     return 0
--- settings.py
+++ settings.py
@@ -8,6 +8,12 @@ load_dotenv()
 def read_bool(value: str) -> bool:
     return value.lower() in ("1", "true")
 
+APP_ENV = os.environ["APP_ENV"]
+APP_HOST = os.environ["APP_HOST"]
+APP_PORT = int(os.environ["APP_PORT"])
+
+CODE_HOTRELOAD = read_bool(os.environ["CODE_HOTRELOAD"])
+LOG_LEVEL = os.environ["LOG_LEVEL"]
 
 AWS_REGION = os.environ["AWS_REGION"]
 AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
