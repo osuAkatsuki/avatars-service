6b78db0a7c7703ce2f194b36f30e876af45b2420
infernalfire72 <jkoe2502@gmail.com>
2024-07-23 00:48:09 +0200

    remove png extension check

--- app/api/v1/clan_icons.py
+++ app/api/v1/clan_icons.py
@@ -58,9 +58,6 @@ async def get_clan_icon(file_path: str):
     if ".." in file_path or "/" in file_path:
         return Response(status_code=404)
 
-    if not file_path.endswith(".png"):
-        file_path += ".png"
-
     download_response = await s3.download(file_path, "clan-icons")
     if download_response is None:
         logging.warning(
